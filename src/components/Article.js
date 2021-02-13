98adbf2f4500f921da13092040755895fbd6766e
roshni-patel <rpatel01@wesleyan.edu>
2021-02-12 22:41:11 -0600

    Copy link button changes text to copied instead of using span with red text

--- src/components/Article.js
+++ src/components/Article.js
@@ -8,8 +8,6 @@ import {
     FacebookShareButton,
     TwitterShareButton,
     WhatsappShareButton,
-    LinkedinShareButton,
-    ViberShareButton
 } from "react-share";
 
 import {
@@ -17,8 +15,6 @@ import {
     FacebookIcon,
     TwitterIcon,
     WhatsappIcon,
-    LinkedinIcon,
-    ViberIcon
 } from "react-share";
 
 
@@ -56,27 +52,22 @@ const Article = (props) => {
         <div className="card h-100">
         <img src={props.image_url} className="card-img-top" alt="news"/>
         <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
+            <h5 className="card-title">{props.source_name}: {props.title}</h5>
             <p className="card-text">{props.description ? props.description : null }</p>
         </div>
         <div className="card-footer">
-        <a href={props.article_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary m-1">Read</a>
-        {/* { props.isSaved ? <button className="btn btn-primary" onClick={() => unsaveArticle(props.userId, props.articleId)}>Unsave</button> : <button className="btn btn-primary m-1" onClick={() => saveArticle(props.userId, props.articleId)}>Save</button> } */}
-            
+        <a href={props.article_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary m-2">Read</a>
         { user ? 
-            props.isSaved ? <button className="btn btn-primary" onClick={() => unsaveArticle(props.userId, props.articleId)}>Unsave</button> : <button className="btn btn-primary m-1" onClick={() => saveArticle(props.userId, props.articleId)}>Save</button>
+            props.isSaved ? <button className="btn btn-primary m-2" onClick={() => unsaveArticle(props.userId, props.articleId)}>Unsave</button> : <button className="btn btn-primary m-2" onClick={() => saveArticle(props.userId, props.articleId)}>Save</button>
             : null 
         }
             <CopyToClipboard text={props.article_url} onCopy={() => setCopied(true)}>
-            <button className="btn btn-primary m-1">Copy Link</button>
+            { copied ?  <button className="btn btn-primary m-2">Copied!</button> : <button className="btn btn-primary m-2">Copy Link</button>}
             </CopyToClipboard>
-            {copied ? <span style={{ color: "red" }}>Copied!</span> : null}
-            <EmailShareButton url={props.article_url}><EmailIcon size={40} className="m-1" /></EmailShareButton>
-            <FacebookShareButton url={props.article_url} quote={props.title}><FacebookIcon size={40} className="m-1"/></FacebookShareButton>
-            <TwitterShareButton url={props.article_url}><TwitterIcon size={40} className="m-1"/></TwitterShareButton>
-            <WhatsappShareButton url={props.article_url}><WhatsappIcon size={40} className="m-1"/></WhatsappShareButton>
-            <LinkedinShareButton url={props.article_url}><LinkedinIcon size={40} className="m-1"/></LinkedinShareButton>
-            <ViberShareButton url={props.article_url}><ViberIcon size={40} className="m-1"/></ViberShareButton>
+            <EmailShareButton url={props.article_url}><EmailIcon size={40} className="m-2" /></EmailShareButton>
+            <FacebookShareButton url={props.article_url} quote={props.title}><FacebookIcon size={40} className="m-2"/></FacebookShareButton>
+            <TwitterShareButton url={props.article_url}><TwitterIcon size={40} className="m-2"/></TwitterShareButton>
+            <WhatsappShareButton url={props.article_url}><WhatsappIcon size={40} className="m-2"/></WhatsappShareButton>
         </div>
         </div>
         </div>
--- src/components/SavedArticle.js
+++ src/components/SavedArticle.js
@@ -5,7 +5,6 @@ import {
     FacebookShareButton,
     TwitterShareButton,
     WhatsappShareButton,
-    LinkedinShareButton
 } from "react-share";
 
 import {
@@ -13,7 +12,6 @@ import {
     FacebookIcon,
     TwitterIcon,
     WhatsappIcon,
-    LinkedinIcon
 } from "react-share";
 
 const SavedArticle = (props) => {
@@ -24,21 +22,19 @@ const SavedArticle = (props) => {
         <div className="card h-100">
         <img src={props.image_url} className="card-img-top" alt="news"/>
         <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
+            <h5 className="card-title">{props.source_name}: {props.title}</h5>
             <p className="card-text">{props.description ? props.description : null }</p>
         </div>
         <div className="card-footer">
-            <a href={props.article_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary m-1">Read</a>
-            <button className="btn btn-primary m-1" onClick={() => props.unsaveArticle(props.userId, props.articleId)}>Unsave</button>
+            <a href={props.article_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary m-2">Read</a>
+            <button className="btn btn-primary m-2" onClick={() => props.unsaveArticle(props.userId, props.articleId)}>Unsave</button>
             <CopyToClipboard text={props.article_url} onCopy={() => setCopied(true)}>
-            <button className="btn btn-primary m-1">Copy Link</button>
+                { copied ?  <button className="btn btn-primary m-2">Copied!</button> : <button className="btn btn-primary m-2">Copy Link</button>}
             </CopyToClipboard>
-            {copied ? <span style={{ color: "red" }}>Copied!</span> : null}
-            <EmailShareButton url={props.article_url}><EmailIcon size={40} className="m-1" /></EmailShareButton>
-            <FacebookShareButton url={props.article_url} quote={props.title}><FacebookIcon size={40} className="m-1"/></FacebookShareButton>
-            <TwitterShareButton url={props.article_url}><TwitterIcon size={40} className="m-1"/></TwitterShareButton>
-            <WhatsappShareButton url={props.article_url}><WhatsappIcon size={40} className="m-1"/></WhatsappShareButton>
-            <LinkedinShareButton url={props.article_url}><LinkedinIcon size={40} className="m-1"/></LinkedinShareButton>
+            <EmailShareButton url={props.article_url}><EmailIcon size={40} className="m-2" /></EmailShareButton>
+            <FacebookShareButton url={props.article_url} quote={props.title}><FacebookIcon size={40} className="m-2"/></FacebookShareButton>
+            <TwitterShareButton url={props.article_url}><TwitterIcon size={40} className="m-2"/></TwitterShareButton>
+            <WhatsappShareButton url={props.article_url}><WhatsappIcon size={40} className="m-2"/></WhatsappShareButton>
         </div>
         </div>
         </div>
