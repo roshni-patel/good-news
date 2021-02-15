0eab2fc8fef0121d3213d1e171ca31537e5b3171
roshni-patel <rpatel01@wesleyan.edu>
2021-02-14 19:37:19 -0600

    Fixed proxy heroku api url

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -18,6 +18,19 @@ const UserProvider = (props) => {
         return;
       }
 
+      // var docRef = firebase.firestore().collection('users').doc(user.uid);
+
+      // docRef.get().then((doc) => {
+      //     if (doc.exists) {
+      //         console.log("Document data:", doc.data());
+      //     } else {
+      //         // doc.data() will be undefined in this case
+      //         console.log("No such document!");
+      //     }
+      // }).catch((error) => {
+      //     console.log("Error getting document:", error);
+      // });
+
     firebase.firestore().collection('users').doc(user.uid).set({
       displayName: user.displayName,
       email: user.email,
